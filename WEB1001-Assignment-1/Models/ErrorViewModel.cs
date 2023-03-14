cf1c2d6c5f4ab0741d13db2f40b5ebd77ee92d5f
Zachary <zdean1995@gmail.com>
2023-03-14 05:00:53 -0400

    Application Created. No files edited yet.

---
+++ WEB1001-Assignment-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB1001_Assignment_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
